e180789fa1129127532153f81dbe8bc94230d67a
Ahmed Akram <58849502+Akramovic1@users.noreply.github.com>
2022-01-09 23:48:19 +0200

    add book and remove book pages added

---
+++ src/main/java/com/example/bookstore/addBookController.java
@@ -0,0 +1,31 @@
+package com.example.bookstore;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class addBookController {
+    @FXML
+    private Button add_btn;
+
+    @FXML
+    private TextField isbn;
+
+    @FXML
+    private TextField title;
+
+    @FXML
+    private TextField author;
+
+    @FXML
+    private TextField publisher;
+
+    @FXML
+    private TextField year;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private TextField cateogry;
+}
