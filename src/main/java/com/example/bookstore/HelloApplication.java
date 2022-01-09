904f8c78374b0c2e532817c1c500f1f5fe6472dd
Ahmed Akram <58849502+Akramovic1@users.noreply.github.com>
2022-01-09 22:58:27 +0200

    delete

--- src/main/java/com/example/bookstore/HelloApplication.java
+++ src/main/java/com/example/bookstore/HelloApplication.java
@@ -13,7 +13,7 @@ import java.sql.*;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1315, 890);
         stage.setTitle("BOOK STORE");
         stage.setScene(scene);
