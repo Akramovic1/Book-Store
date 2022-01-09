5357373fc93ec4cc2203f4a7d5942d0835617757
Rana Ayman <ranaayman35.ra@gmail.com>
2022-01-09 23:49:20 +0200

    Payment status message

---
+++ src/main/java/com/example/bookstore/model/paymentFailController.java
@@ -0,0 +1,5 @@
+package com.example.bookstore.model;
+
+public class paymentFailController {
+    //Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+}
---
+++ src/main/java/com/example/bookstore/model/paymentSuccessController.java
@@ -0,0 +1,5 @@
+package com.example.bookstore.model;
+
+public class paymentSuccessController {
+    //Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+}
