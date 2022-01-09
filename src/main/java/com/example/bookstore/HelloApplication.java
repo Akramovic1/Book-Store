01ed9c1ed9e5e58502e3789f5bbcc8f672c29bbf
Rana Ayman <ranaayman35.ra@gmail.com>
2022-01-09 23:50:43 +0200

    some change

--- src/main/java/com/example/bookstore/HelloApplication.java
+++ src/main/java/com/example/bookstore/HelloApplication.java
@@ -13,7 +13,7 @@ import java.sql.*;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("paymentSuccess.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1315, 890);
         stage.setTitle("BOOK STORE");
         stage.setScene(scene);
