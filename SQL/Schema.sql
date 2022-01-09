554aa608bf8fb4a1555942524cf2a3be38ef52e4
Rana Ayman <ranaayman35.ra@gmail.com>
2022-01-09 13:13:56 +0200

    3 Triggers added
    Note:
    Shipping_address in book_order table is ignored with a comment until it's needed.

--- SQL/Schema.sql
+++ SQL/Schema.sql
@@ -55,24 +55,24 @@ create table user_info
   user_id    int primary key auto_increment,
   user_name  varchar(50) NOT NULL,
   password   varchar(255) NOT NULL,
-  first_name varchar(50) NOT NULL,
-  last_name  varchar(50) NOT NULL,
+  first_name varchar(50),
+  last_name  varchar(50),
   email      varchar(255) NOT NULL UNIQUE,
-  phone      varchar(20) NOT NULL,
-  Shipping_address varchar(100) NOT NULL,
+  phone      varchar(20),
+  Shipping_address varchar(100),
   privilege  boolean NOT NULL
 );
 
 create table book_order
 (
   order_id      int auto_increment primary key,
-  customer_id   int NOT NULL,
+  user_id   int NOT NULL,
   isbn          int NOT NULL,
   quantity      int NOT NULL,
-  Shipping_address varchar(100) NOT NULL,
+  -- Shipping_address varchar(100),
   date          date ,
   foreign key (isbn) references book (isbn) ON UPDATE CASCADE ON DELETE CASCADE,
-  foreign key (customer_id) references user_info (user_id) ON UPDATE CASCADE ON DELETE CASCADE
+  foreign key (user_id) references user_info (user_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 create table credit_card
@@ -86,3 +86,49 @@ create table credit_card
 
 alter table book
   add foreign key (publisher_id) references publisher (publisher_id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+/*************************** Triggers ******************************/
+delimiter $$
+create trigger modify_existing_books before update  on book
+    for each row
+begin
+    if  new.num_of_copies<0 then
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot add or update book row with number of copies < 0';
+end if;
+end;$$
+
+-- Test modify_existing_books :
+
+-- insert into publisher (publisher_id,publisher_name) values(1,"publisher name");
+-- insert into book (isbn,title,publisher_id,publication_year,selling_price,category,num_of_copies,threshold)values("1","title","1",YEAR(CURDATE()),"10","Science","1","5");
+-- update book set num_of_copies = num_of_copies-2 where isbn = 1 ;
+
+create trigger place_orders_books after update on book 
+for each row
+begin
+    if old.num_of_copies>new.threshold and new.num_of_copies<=new.threshold then
+            insert into book_order(user_id,isbn,quantity,date) values("1",new.isbn,new.threshold*2,CURDATE());
+    end if;
+end;
+
+-- Test place_orders_books :
+
+-- insert into user_info (user_id,user_name,password,email,privilege)values("1","manager","******","manager@gmail.com",true);
+-- insert into publisher (publisher_id,publisher_name) values(1,"publisher name");
+-- insert into book (isbn,title,publisher_id,publication_year,selling_price,category,num_of_copies,threshold)values("1","title","1",YEAR(CURDATE()),"10","Science","6","5");
+-- update book set num_of_copies = num_of_copies-2 where isbn = 1 ;
+
+create trigger confirm_orders before  delete on book_order
+    for each row
+begin
+    update book set num_of_copies = num_of_copies + old.quantity where isbn = old.isbn ;
+end;
+
+-- Test confirm_orders :
+
+-- insert into user_info (user_id,user_name,password,email,privilege)values("1","manager","******","manager@gmail.com",true);
+-- insert into publisher (publisher_id,publisher_name) values(1,"publisher name");
+-- insert into book (isbn,title,publisher_id,publication_year,selling_price,category,num_of_copies,threshold)values("1","title","1",YEAR(CURDATE()),"10","Science","6","5");
+-- update book set num_of_copies = num_of_copies-1 where isbn = 1 ;
+-- delete from book_order where order_id="1";
