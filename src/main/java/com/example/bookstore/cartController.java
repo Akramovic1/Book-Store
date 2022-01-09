2b51089325e1936efb631d2703bfa3d440211f04
Ahmed Akram <58849502+Akramovic1@users.noreply.github.com>
2022-01-09 19:55:06 +0200

    some fixes

--- src/main/java/com/example/bookstore/cartController.java
+++ src/main/java/com/example/bookstore/cartController.java
@@ -2,16 +2,21 @@ package com.example.bookstore;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 
 public class cartController {
-    
+
     @FXML
     private ScrollPane cartPane;
+
+    @FXML
+    private Button addBookCart_btn;
 
     @FXML
     private Button removeBookCart_btn;
 
     @FXML
-    private Button addBookCart_btn;
+    private Label totalPrice;
+    
 }
