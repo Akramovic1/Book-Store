2369263d3e647bf88ae1129a2f05294b83d80bfa
Ahmed Akram <58849502+Akramovic1@users.noreply.github.com>
2022-01-09 19:48:41 +0200

    Cart GUI added

---
+++ src/main/java/com/example/bookstore/cartController.java
@@ -0,0 +1,17 @@
+package com.example.bookstore;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class cartController {
+    
+    @FXML
+    private ScrollPane cartPane;
+
+    @FXML
+    private Button removeBookCart_btn;
+
+    @FXML
+    private Button addBookCart_btn;
+}
