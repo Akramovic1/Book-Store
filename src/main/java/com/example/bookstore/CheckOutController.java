bcc2508b6864b004793432225066c459ad18fd7b
Momen Ibrahim Fawzy <momenibrahimfawzy982000@gmail.com>
2022-01-10 15:58:50 +0200

    checkoutcontroller

--- src/main/java/com/example/bookstore/CheckOutController.java
+++ src/main/java/com/example/bookstore/CheckOutController.java
@@ -1,30 +1,64 @@
 package com.example.bookstore;
 
+import com.example.bookstore.dao.DBO;
+import com.example.bookstore.model.Book;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class CheckOutController {
-//Scene scene = new Scene(fxmlLoader.load(), 750, 520);
     @FXML
     private TextField Cardholder_name;
-
     @FXML
     private TextField Card_number;
-
     @FXML
     private TextField Expiry_date;
-
     @FXML
     private TextField CVV;
-
     @FXML
     private Button payBtn;
+    private UserSession userSession;
+
+    public CheckOutController() {
+    }
+
+    public void initialize() {
+        this.userSession = UserSession.getSession();
+    }
 
     @FXML
     void PayClicked(MouseEvent event) {
-
+        if (this.validate()) {
+            DBO db = new DBO();
+            if (!db.payForBook(this.userSession, Integer.parseInt(this.Card_number.getText()),
+                    this.Expiry_date.getText())) {
+                HashMap<Book, Integer> cart = this.userSession.getCart();
+                StringBuilder sb = new StringBuilder("Book : ");
+                for (Map.Entry<Book, Integer> entry : cart.entrySet())
+                    sb.append(entry.getKey().getTitle()).append(" || ");
+                sb.delete(sb.length() - 4, sb.length());
+                sb.append(" Not Available");
+                userSession.setMessage(sb.toString());
+                HelloApplication.showWindow("paymentFail", "Payment Fail",
+                        500, 500);
+            }
+            else
+                HelloApplication.showWindow("paymentSuccess", "Payment success",
+                        500, 500);
+        }
     }
 
+    private boolean validate() {
+        if (this.Card_number.getText() != null && this.Expiry_date != null) {
+            return true;
+        } else {
+            HelloApplication.showErrorMessage("Incorrect Data");
+            return false;
+        }
+    }
 }
--- src/main/java/com/example/bookstore/HelloApplication.java
+++ src/main/java/com/example/bookstore/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.bookstore;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,26 @@ public class HelloApplication extends Application {
         stage.setTitle("BOOK STORE");
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public static void showWindow(String newWindow, String title, int v1, int v2) {
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(newWindow));
+            Scene scene = new Scene(fxmlLoader.load(), v1, v2);
+            stage.setTitle(title);
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void showErrorMessage(String errorMessage){
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Invalid Operation");
+        errorAlert.setContentText(errorMessage);
+        errorAlert.showAndWait();
     }
 
     public static void main(String[] args) {
