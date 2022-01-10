286e64d5983d64a5a450323547a5ed060c8763dc
Momen Ibrahim Fawzy <momenibrahimfawzy982000@gmail.com>
2022-01-10 12:15:13 +0200

    searching book methods

--- src/main/java/com/example/bookstore/dao/selectingInterface.java
+++ src/main/java/com/example/bookstore/dao/selectingInterface.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public interface selectingInterface {
     Publisher getPublisher(int publisher_id);
     Author getAuthor(int author_id);
+    List<Author> getAuthorByName(String author_Name);
     List<Author> getAuthorsOfBook(int isbn);
     Book getBook(int isbn);
     List<Book> getBooksByTitle(String title);
@@ -18,6 +19,11 @@ public interface selectingInterface {
     List<Book> searchBookWithISBNAndTittle(Integer isbn,String title);
     //Map of Attribute : value of that attribute
     List<Book> searchBookWithCategoryAuthorPublisherName(Map<String,String> hash);
+    List<Book> searchBooksByAuthor(Author author);
+    List<Book> searchBooksByCategory(String category);
+    List<Book> searchBooksByPublisher(Publisher publisher);
+    List<Book> searchBooksByAuthorName(String author_Name);
+
 
 
 }
